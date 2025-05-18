cc8d27c242dcb0c7f0960db185d201ee1b70c33e
kardinalmarkov <kardinal.markov@gmail.com>
2025-05-18 15:10:38 +0300

    10

--- pages/news.js
+++ pages/news.js
@@ -1,12 +1,9 @@
-import Head from 'next/head'
-
 export default function News() {
   return (
-    <>
-      <Head>
-        <title>Новости | Terra Zetetica</title>
-      </Head>
-        <h1>Новости</h1>
-        <p>Скоро здесь появятся анонсы и отчёты о наших проектах.</p>
+    <section className="wrapper">
+      <h1>Новости</h1>
+      <p>Здесь будет лента последних публикаций и событий Terra Zetetica.</p>
+      {/* TODO: подключить компонент постов или внешний API */}
+    </section>
   )
 }
