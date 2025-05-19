72d5c3cd0c35aa3e8e1083c28cd3ca638fe51cbf
kardinalmarkov <kardinal.markov@gmail.com>
2025-05-19 07:34:46 +0300

    verify.html

--- pages/constitution.js
+++ pages/constitution.js
@@ -9,7 +9,7 @@ export default function Constitution() {
       <main className="wrapper" style={{ paddingTop: '1rem' }}>
         <h1>Конституция Terra Zetetica</h1>
         <p style={{ marginBottom: '1.5rem' }}>
-          Полный текст Конституции доступен по ссылке:
+          Полный текст Конституции доступен в IPFS хранилище по ссылке: https://tomato-eligible-lizard-8.mypinata.cloud/ipfs/bafybeiexp532nzeuxwatndcnt2dhxphhb6ncfdwjulddjkppkie2zcgw5q
         </p>
         <a
           className="btn primary"
@@ -17,7 +17,7 @@ export default function Constitution() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          📜 Открыть в IPFS
+          📜 Открыть текст Конституции в IPFS
         </a>
       </main>
     </>
