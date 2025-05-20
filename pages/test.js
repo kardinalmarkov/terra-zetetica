d5c0aaa78b69fd067b0f704cf6d812cc4ded906e
kardinalmarkov <kardinal.markov@gmail.com>
2025-05-20 17:01:46 +0300

    test

---
+++ pages/test.js
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Тестовая страница для шлифовки</h1>
+      <p>Здесь можно проверить любые компоненты, не выводя ссылку в меню.</p>
+    </main>
+  )
+}
