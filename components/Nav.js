6bc6eef0b09bf096ebed1e59ceacbb9d93ea875a
kardinalmarkov <kardinal.markov@gmail.com>
2025-05-21 21:29:41 +0300

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -7,15 +7,15 @@ import LangSwitch from './LangSwitch'
 export default function Nav() {
   const { locale } = useRouter()
   const t = {
-    home:         locale === 'ru' ? 'Главная'        : 'Home',
-    about:        locale === 'ru' ? 'О государстве'  : 'About',
-    news:         locale === 'ru' ? 'Новости'        : 'News',
-    constitution: locale === 'ru' ? 'Конституция'    : 'Constitution',
-    faq:          locale === 'ru' ? 'FAQ'            : 'FAQ',
-    roadmap:      locale === 'ru' ? 'Дорожная карта' : 'Roadmap',
-    materials:    locale === 'ru' ? 'Материалы'      : 'Materials',
-    contacts:     locale === 'ru' ? 'Контакты'       : 'Contacts',
-    apply:        locale === 'ru' ? 'Стать гражданином' : 'Become a citizen',
+    home:         locale === 'ru' ? 'Главная'          : 'Home',
+    about:        locale === 'ru' ? 'О государстве'    : 'About',
+    // news удалён
+    constitution: locale === 'ru' ? 'Конституция'      : 'Constitution',
+    faq:          locale === 'ru' ? 'Вопросы'          : 'FAQ',
+    roadmap:      locale === 'ru' ? 'Дорожная карта'   : 'Roadmap',
+    materials:    locale === 'ru' ? 'Материалы'        : 'Materials',
+    contacts:     locale === 'ru' ? 'Контакты'         : 'Contacts',
+    apply:        locale === 'ru' ? 'Стать гражданином': 'Become a citizen',
   }
 
   return (
@@ -27,7 +27,6 @@ export default function Nav() {
       <nav className={styles.menu}>
         <Link href="/">{t.home}</Link>
         <Link href="/about">{t.about}</Link>
-        <Link href="/news">{t.news}</Link>
         <Link href="/constitution">{t.constitution}</Link>
         <Link href="/faq">{t.faq}</Link>
         <Link href="/roadmap">{t.roadmap}</Link>
