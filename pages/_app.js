15c9b03cc9256b0b2416c8f94401212439e04bf9
kardinalmarkov <kardinal.markov@gmail.com>
2025-05-22 13:13:36 +0300

    1

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import { appWithTranslation } from 'next-i18next'
 import Nav from '../components/Nav'
@@ -6,6 +7,11 @@ import Footer from '../components/Footer'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.webp" type="image/webp" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Terra Zetetica</title>
+      </Head>
       <Nav />
       <Component {...pageProps} />
       <Footer />
