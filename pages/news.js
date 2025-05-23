585c1d91065d9560cb23cca8eb8384e103230054
kardinalmarkov <kardinal.markov@gmail.com>
2025-05-23 18:07:27 +0300

    Update news.js

--- pages/news.js
+++ pages/news.js
@@ -1,9 +1,40 @@
+// pages/news.js
+import Head from 'next/head'
+
+const videos = [
+  'b800908bda2146b3b85a0fc8578ee90f', // пример – можно расширять массив
+]
+
 export default function News() {
   return (
-    <section className="wrapper">
-      <h1>Новости</h1>
-      <p>Здесь будет лента последних публикаций и событий Terra Zetetica.</p>
-      {/* TODO: подключить компонент постов или внешний API */}
-    </section>
+    <main className="wrapper">
+      <Head>
+        <title>Новости | Terra Zetetica</title>
+      </Head>
+
+      <h1>Новости Terra Zetetica</h1>
+      <p>Последние объявления, видеоролики и события нашего государства.</p>
+
+      {videos.map(id => (
+        <div key={id} style={{ margin: '2rem 0', position: 'relative', paddingTop: '56.25%' }}>
+          <iframe
+            src={`https://app.heygen.com/embeds/${id}`}
+            title="HeyGen video player"
+            allow="encrypted-media; fullscreen;"
+            allowFullScreen
+            style={{
+              position: 'absolute',
+              inset: 0,
+              width: '100%',
+              height: '100%',
+              border: 'none',
+              borderRadius: 8
+            }}
+          />
+        </div>
+      ))}
+
+      {/* TODO: сюда легко вставить компонент «PostList» с бек-API */}
+    </main>
   )
 }
